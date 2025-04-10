fe7eade73a76d7c59df3756ea925243c79f3b02d
Yehtutkhant <williamkhant4@gmail.com>
2025-04-10 17:09:22 +0700

    prisma schema updated

--- Kafka-app-server/consumer.ts
+++ Kafka-app-server/consumer.ts
@@ -1,34 +1,32 @@
-import {Kafka} from 'kafkajs';
-import { PrismaClient } from './prisma/generated/prisma';
+import { Kafka } from "kafkajs";
+import { PrismaClient } from "./prisma/generated/prisma";
 
-const kafka= new Kafka({
-    clientId: 'my-app',
-    brokers: ['localhost:9092']
+const kafka = new Kafka({
+	clientId: "my-app",
+	brokers: ["localhost:9092"],
 });
 
-const consumer= kafka.consumer({groupId: "test-group"});
-const prisma= new PrismaClient();
+const consumer = kafka.consumer({ groupId: "test-group" });
+const prisma = new PrismaClient();
 
-const run= async ()=>{
-    await consumer.connect();
-    await consumer.subscribe({topic: "test", fromBeginning: true});
-    await consumer.run({
-        eachMessage: async({topic, partition, message})=>{
-            console.log(message);
-            if(message.value){
-                const {name, password}= JSON.parse(message.value.toString())
-            const createNull = await prisma.user.create({
-                data: {
-                  username: name,
-                  password
-                },
-              })
-              console.log(createNull);
-            }   
-        }
-    })
-}
+const run = async () => {
+	await consumer.connect();
+	await consumer.subscribe({ topic: "test", fromBeginning: true });
+	await consumer.run({
+		eachMessage: async ({ topic, partition, message }) => {
+			console.log(message);
+			if (message.value) {
+				const { name, password } = JSON.parse(message.value.toString());
+				const createNull = await prisma.user.create({
+					data: {
+						username: name,
+						password,
+					},
+				});
+				console.log(createNull);
+			}
+		},
+	});
+};
 
 run().catch(console.error);
-
-//added smth
